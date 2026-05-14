797f1d3a55aeb63428b4a63554611b77aeaafbc0
Lucas Baruffi <lucasbaruffi6@gmail.com>
2026-05-14 14:14:00 -0300

    docs: documentado o

--- Program.cs
+++ Program.cs
@@ -1,22 +1,27 @@
+// Inicia o criador da aplicação
 var builder = WebApplication.CreateBuilder(args);
 
+// Define quais configurações terá, com a OpennAPI e Swagger
 builder.Services.AddOpenApi();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Com as configurações, constrói o app
 var app = builder.Build();
 
+// Se estiver em desenvolvimento, mostra o Swagger(?)
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Ainda não sei o que é isso
 // app.UseHttpsRedirection();
 
+// Função de exemplo ao criar o projeto, que retorna dados de clima falsos.
+// E pode ser usada como base para os demais endpoints.
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool",
@@ -38,14 +43,18 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+// Endpoint criado como teste, pode ser excluído posteriormente
 app.MapGet("/teste", () =>
 {
     var json = new { teste = "valor de teste" };
     return json;
 });
 
+// Após todas as configurações, executa o App
 app.Run();
 
+// Pelo que entendi, isso é um "tipo", que é utilizado na API do clima
+// Utilizado para Orientação à Objetos.
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
