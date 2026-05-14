febfcb750a6a4a16a580093e2853f157f532d554
Lucas Baruffi <lucasbaruffi6@gmail.com>
2026-05-14 19:19:25 -0300

    feat: dependencias para conexão do banco com o C#

--- obj/Debug/net10.0/lojinha.AssemblyInfo.cs
+++ obj/Debug/net10.0/lojinha.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lojinha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8989ef3f658d44136e7dcad46c708e69555fa0ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f5861bb5e24a49666145915f7e4a8319fc220f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("lojinha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lojinha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
