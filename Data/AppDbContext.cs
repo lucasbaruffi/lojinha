7a1e523297e286f837b15c5704ed995a2e94975c
Lucas Baruffi <lucasbaruffi6@gmail.com>
2026-05-15 09:27:55 -0300

    feat: iniciando

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+// Basicamente importa a "biblioteca" do EF, 
+// para saber o que é DbContext, DbContext, DbContextOptions ...
+using Microsoft.EntityFrameworkCore;
+
+// Criando a classe AppDbContext que tem o mesmo tipo que DbContext
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+        // Pelo que entendi esse DbContext será basicamente a conexão com o 
+        // banco de dados, aqui passaremos as configurações, como onde está
+        // o banco e quais os dados de acesso.
+    }
+
+    // Representa a tabela de Clientes:
+    public DbSet<Cliente> Clientes { get; set; } = null!; 
+}
--- obj/Debug/net10.0/lojinha.AssemblyInfo.cs
+++ obj/Debug/net10.0/lojinha.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lojinha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+febfcb750a6a4a16a580093e2853f157f532d554")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282fc79a00311c4c3fc02d78cda79533b78169f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("lojinha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lojinha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
