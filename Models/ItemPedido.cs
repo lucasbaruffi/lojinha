8e8f887404cf34034258bc62ef1c7da51a806d11
Lucas Baruffi <lucasbaruffi6@gmail.com>
2026-05-18 15:41:59 -0300

    fix: retirado set de ID do item

--- Models/ItemPedido.cs
+++ Models/ItemPedido.cs
@@ -1,6 +1,6 @@
 public class ItemPedido
 {
-    public int Id { get; set; }
+    public int Id { get; }
     public int IdPedido { get; set; }
     public string Nome { get; set; } = string.Empty;
     public decimal ValorUnitario { get; set; }
