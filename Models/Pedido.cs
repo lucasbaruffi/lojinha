d1d3d1f4a4cf34e8fe80509cbf95c640384ce52e
Lucas Baruffi <lucasbaruffi6@gmail.com>
2026-05-18 16:16:35 -0300

    refactor: começando a alterar lógica para que pedido tenha ao menos um item

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -2,5 +2,6 @@ public class Pedido
 {
     public int Id { get; }
     public int IdCliente { get; set; }
-    public DateTime DtPedido { get; set; }
+    public DateTime DtPedido { get; }
+    public required List<ItemPedido> Itens { get; set; }
 }
