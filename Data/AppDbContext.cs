3efc7b8be66a69ac7527f2934fe582dbaff5e655
Lucas Baruffi <lucasbaruffi6@gmail.com>
2026-05-18 09:15:10 -0300

    feat: feito mapeamento básico de tabelas e colunas de pedidos e items

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,13 +9,14 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {
-        // Pelo que entendi esse DbContext será basicamente a conexão com o 
-        // banco de dados, aqui passaremos as configurações, como onde está
-        // o banco e quais os dados de acesso.  
+        // As configurações de acesso do banco estão em appsettings.Development.json
     }
 
-    // Representa a tabela de Clientes:
+    // Representa as tabelas
+    // O que está entre <> é a classe criada nos Models
     public DbSet<Cliente> Clientes { get; set; } = null!; 
+    public DbSet<Pedido> Pedidos { get; set; } = null!;
+    public DbSet<ItemPedido> ItemsPedidos { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -45,6 +46,46 @@ public class AppDbContext : DbContext
 
         cliente.Property(x => x.Nome)
             .HasColumnName("nome");
+
+
+        // --------- TRABALHA EM PEDIDOS ---------
+        var pedido = modelBuilder.Entity<Pedido>();
+        
+        // Diz o nome real da tabela
+        pedido.ToTable("pedidos");
+
+        // Identifica cada campo com as colunas da tabela
+        pedido.Property(x => x.Id)
+            .HasColumnName("id");
+
+        pedido.Property(x => x.IdCliente)
+            .HasColumnName("id_cliente");
+
+        pedido.Property(x => x.DtPedido)
+            .HasColumnName("dt_pedido");
+
+
+        // --------- TRABALHA EM PEDIDOS ---------
+        var ItemPedido = modelBuilder.Entity<ItemPedido>();
+        
+        // Diz o nome real da tabela
+        ItemPedido.ToTable("item_pedido");
+
+        // Identifica cada campo com as colunas da tabela
+        ItemPedido.Property(x => x.Id)
+            .HasColumnName("id");
+
+        ItemPedido.Property(x => x.IdPedido)
+            .HasColumnName("id_pedido");
+
+        ItemPedido.Property(x => x.Nome)
+            .HasColumnName("nome");
+        
+        ItemPedido.Property(x => x.ValorUnitario)
+            .HasColumnName("valor_unitario");
+
+        ItemPedido.Property(x => x.Quantidade)
+            .HasColumnName("quantidade");
     }
 
 }
