fdd571927ae7aff95d3d9031f193424b968240a9
Lucas Baruffi <lucasbaruffi6@gmail.com>
2026-05-19 09:58:31 -0300

    feat: primeiras requisições de clientes

--- wwwroot/js/clientes.js
+++ wwwroot/js/clientes.js
@@ -26,3 +26,40 @@ async function carregarClientes() {
 }
 
 carregarClientes();
+
+
+const formCliente = document.getElementById("form-cliente");
+
+// Fica escutando o envio do formulário, é ativado ao clicar.
+formCliente.addEventListener("submit", async function (event) {
+
+    event.preventDefault();
+
+    const cliente = {
+        nome: document.getElementById("nome").value,
+        cpf: document.getElementById("cpf").value,
+        email: document.getElementById("email").value,
+        dtNascimento: document.getElementById("dtNascimento").value,
+        endereco: document.getElementById("endereco").value
+    };
+
+    const resposta = await fetch("/api/clientes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(cliente)
+    });
+
+    if (resposta.ok) {
+        alert("Cliente cadastrado com sucesso!");
+        formCliente.reset();
+        carregarClientes();
+    }
+    else {
+        // Se der erro, mostra um popup
+        // TODO tirar popup e inserir erro no HTML
+        const erro = await resposta.json();
+        alert(erro.mensagem);
+    }
+});
